94f8c723b2f2f1ef7d05051a643c589fa07ded26
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-06-02 10:02:01 +0600

    added member delete api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ev60phs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,6 +36,12 @@ async function run() {
 
 
     // users related api
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    } );
+
+
     app.post('/users', async(req, res) =>{
       const user = req.body;
        // insert email if user doesnt exists: 
@@ -47,6 +53,16 @@ async function run() {
        
       const result = await userCollection.insertOne(user);
       res.send(result)
+    })
+
+    // make admin
+    
+
+    app.delete('/users/:id', async( req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result); 
     })
 
 

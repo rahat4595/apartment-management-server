0dbbe2b85e4c44ffa743542b62f472e23795e09d
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-06-04 11:48:02 +0600

    installed stripe and added key

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
